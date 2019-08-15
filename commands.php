e08f80b7d65091db04e456c6fdbc8732e1fd8f66
Febriyanto <febriyanto@panorama-jtb.com>
2019-08-15 11:01:44 +0700

    subtract command

--- commands.php
+++ commands.php
@@ -1,12 +1,6 @@
 <?php
 
 return [
-    \Src\Commands\AddCommand::class,
     \Src\Commands\SubtractCommand::class,
-    \Src\Commands\DivideCommand::class,
-    \Src\Commands\MultiplyCommand::class,
-    \Src\Commands\PowCommand::class,
-    \Src\Commands\HistoryCommand::class,
-    \Src\Commands\HistoryDeleteCommand::class,
     // \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
 ];
