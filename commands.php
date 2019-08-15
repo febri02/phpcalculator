63a715a0f207784f0b36703f3ff0f23b4e872fc1
Febriyanto <febriyanto@panorama-jtb.com>
2019-08-15 10:59:58 +0700

    add command

--- commands.php
+++ commands.php
@@ -2,11 +2,5 @@
 
 return [
     \Src\Commands\AddCommand::class,
-    \Src\Commands\SubtractCommand::class,
-    \Src\Commands\DivideCommand::class,
-    \Src\Commands\MultiplyCommand::class,
-    \Src\Commands\PowCommand::class,
-    \Src\Commands\HistoryCommand::class,
-    \Src\Commands\HistoryDeleteCommand::class,
     // \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
 ];
